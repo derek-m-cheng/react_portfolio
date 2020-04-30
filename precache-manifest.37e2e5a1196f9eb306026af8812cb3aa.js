903d5092254c0a84cf94785ada730245a7b2ee8a
Derek Cheng <dcheng@umail.ucsb.edu>
2020-04-29 21:11:39 -0700

    Updates

---
+++ precache-manifest.37e2e5a1196f9eb306026af8812cb3aa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6dc6b6718328e8ee49ee3561760e87",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "d49cecb62ee65194d53d",
+    "url": "/react_portfolio/static/css/main.8d0e5338.chunk.css"
+  },
+  {
+    "revision": "5ad8b9b0eabbceafe8a2",
+    "url": "/react_portfolio/static/js/2.f0f36803.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/react_portfolio/static/js/2.f0f36803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49cecb62ee65194d53d",
+    "url": "/react_portfolio/static/js/main.805c27f7.chunk.js"
+  },
+  {
+    "revision": "7ce6d244c024b2bdf428",
+    "url": "/react_portfolio/static/js/runtime-main.242ebcdf.js"
+  },
+  {
+    "revision": "12721f496b9ae493786140bc58d8e9be",
+    "url": "/react_portfolio/static/media/centralpark.12721f49.jpg"
+  },
+  {
+    "revision": "2850a7c85b2f83a00dc523bfe6588073",
+    "url": "/react_portfolio/static/media/me.2850a7c8.jpg"
+  },
+  {
+    "revision": "cded9f7437f0934ab2b31fdbc70db985",
+    "url": "/react_portfolio/static/media/met.cded9f74.jpg"
+  },
+  {
+    "revision": "90046a0cadcd729819df4c8b5f373b6b",
+    "url": "/react_portfolio/static/media/met2.90046a0c.jpg"
+  },
+  {
+    "revision": "269bddc0f87f4f92b0bf3580bf984325",
+    "url": "/react_portfolio/static/media/met3.269bddc0.jpg"
+  },
+  {
+    "revision": "0d5461b26c491bda7c3c9c0fa360f377",
+    "url": "/react_portfolio/static/media/shaver.0d5461b2.jpg"
+  },
+  {
+    "revision": "14ee798f675e7f9ea3d1b2b7bd18523d",
+    "url": "/react_portfolio/static/media/shaver2.14ee798f.jpg"
+  },
+  {
+    "revision": "a53ce58d435a1010813b4652cabd88aa",
+    "url": "/react_portfolio/static/media/staff.a53ce58d.jpg"
+  },
+  {
+    "revision": "3618f4a6c5282deed4521a71e11c79c3",
+    "url": "/react_portfolio/static/media/sunrise.3618f4a6.jpg"
+  },
+  {
+    "revision": "09aa316f41f4a78cc51982db4ed8de67",
+    "url": "/react_portfolio/static/media/waffle.09aa316f.jpg"
+  }
+]);
